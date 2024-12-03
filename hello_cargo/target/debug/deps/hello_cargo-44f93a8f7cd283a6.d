48a35cb737fcc7ac005d6c72228393f1e1cffa94
reghuram.k <reghuramk04@gmail.com>
2024-12-03 18:03:50 +0530

    Build the Hello cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-44f93a8f7cd283a6.d
@@ -0,0 +1,5 @@
+/Users/reghuram.k/Desktop/rust-tut/hello_cargo/target/debug/deps/hello_cargo-44f93a8f7cd283a6: src/main.rs
+
+/Users/reghuram.k/Desktop/rust-tut/hello_cargo/target/debug/deps/hello_cargo-44f93a8f7cd283a6.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/reghuram.k/Desktop/rust-tut/hello_cargo/target/debug/hello_cargo: /Users/reghuram.k/Desktop/rust-tut/hello_cargo/src/main.rs
